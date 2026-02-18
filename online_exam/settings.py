85235f551d69674fa5240ae2f10d7284c3ce0975
BalaChakravarthi <balachakravarthidhupati@gmail.com>
2026-02-18 15:15:52 +0530

    second commit

--- online_exam/settings.py
+++ online_exam/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)1)#=+ud@vngv66gq+f0a5hsz$e0(7=+wwz$=xlvn*o0*infp0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
